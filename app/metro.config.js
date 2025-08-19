a9c351cf9150941e79674125600ee27e168889a3
FelixRobb <felix.nunes.robb@gmail.com>
2025-08-19 16:48:50 +0100

    Remove resolution for 'expo-av' and 'uuid' from metro.config.js, streamlining module path configuration.

--- app/metro.config.js
+++ app/metro.config.js
@@ -4,10 +4,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add resolution for 'expo-av' and 'uuid' which were previously in modulePathProxyList
-config.resolver.extraNodeModules = {
-  "expo-av": require.resolve("expo-av"),
-  uuid: require.resolve("react-native-uuid"),
-};
-
 module.exports = config;
